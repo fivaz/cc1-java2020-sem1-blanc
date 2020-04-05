340d4d4ef1d0bf3a583401efad649f5c0465486c
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-04-05 03:33:13 +0200

    5

--- src/main/java/vue/Affichage.java
+++ src/main/java/vue/Affichage.java
@@ -2,6 +2,7 @@ package vue;
 
 import metier.Connector;
 import metier.Statut;
+import org.apache.log4j.Logger;
 import outils.Reseau;
 
 import java.util.ArrayList;
@@ -13,45 +14,41 @@ import java.util.concurrent.Executors;
 public class Affichage {
 
     private Statut statut = Statut.INIT;
+    private Statut previousStatus = Statut.INIT;
+
+    static final Logger logger = Logger.getLogger(Affichage.class);
 
     public void demarrerSynchronisation() {
-        String network = null;
         try {
-            network = tryToConnect();
-            System.out.print("Avancement: " + statut + " -> ");
-            statut = Statut.CONNECT_OK;
-            System.out.print(statut);
+            String network = tryToConnect();
+            try {
+                tryToSync(network);
+            } catch (InterruptedException e) {
+                previousStatus = statut;
+                statut = Statut.TRANSFERT_ERROR;
+                logger.info("Avancement: " + statut + " -> " + previousStatus);
+            }
         } catch (ExecutionException | InterruptedException e) {
-            System.out.print("Avancement: " + statut + " -> ");
+            previousStatus = statut;
             statut = Statut.CONNECT_ERROR;
-            System.out.print(statut);
+            logger.info("Avancement: " + statut + " -> " + previousStatus);
         }
-        System.out.println(" Reseau:" + network);
-        try {
-            tryToSync(network);
-            System.out.print("Avancement: " + statut + " -> ");
-            statut = Statut.TRANSFERT_OK;
-            System.out.print(statut);
-
-        } catch (InterruptedException e) {
-            System.out.print("Avancement: " + statut + " -> ");
-            statut = Statut.TRANSFERT_ERROR;
-            System.out.print(statut);
-        }
-        System.out.println(" Reseau:" + network);
     }
 
     private void tryToSync(String network) throws InterruptedException {
-        System.out.print("Avancement: " + statut + " -> ");
+        previousStatus = statut;
         statut = Statut.TRANSFERT;
-        System.out.println(statut);
+        logger.info("Avancement: " + statut + " -> " + previousStatus + " Reseau:" + network);
         Reseau.synchroViaReseau(network);
+        previousStatus = statut;
+        statut = Statut.TRANSFERT_OK;
+        logger.info("Avancement: " + statut + " -> " + previousStatus + " Reseau:" + network);
     }
 
     private String tryToConnect() throws ExecutionException, InterruptedException {
-        System.out.print("Avancement: " + statut + " -> ");
+        previousStatus = statut;
         statut = Statut.CONNECT;
-        System.out.println(statut);
+        logger.info("Avancement: " + statut + " -> " + previousStatus);
         List<Connector> connections = new ArrayList<>();
         List<String> networks = Reseau.listeDesReseaux();
 //        List<String> networks = Arrays.asList("3G", "3G");
@@ -62,6 +59,9 @@ public class Affichage {
         ExecutorService connectionExecutor = Executors.newCachedThreadPool();
         String connectionResult = connectionExecutor.invokeAny(connections);
         connectionExecutor.shutdown();
+        previousStatus = statut;
+        statut = Statut.CONNECT_OK;
+        logger.info("Avancement: " + statut + " -> " + previousStatus + " Reseau:" + connectionResult);
         return connectionResult;
     }
 }
